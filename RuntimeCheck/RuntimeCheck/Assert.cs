a549df5b89b402067c82e041ae68377c27bc88e9
LaichR <46196385+LaichR@users.noreply.github.com>
2019-10-14 10:17:06 +0200

    implementation of basic functionality
    - ViolatedAssertionException
    - ViolatedContractException
    - class ExceptionHelper
    - class Assert
    - class Contract
    a first UnitTest Project

---
+++ RuntimeCheck/RuntimeCheck/Assert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace RuntimeCheck
+{
+    public static class Assert
+    {
+        public static void True(bool condition, string message, params object[] args)
+        {
+            if (!condition)
+            {
+                throw ExceptionHelper.CreateException<ViolatedAssertionException>(message, args);
+            }
+        }
+
+        public static void True<T>(bool condition, string message, params object[] args) where T:Exception
+        {
+            if(!condition)
+            {
+                throw ExceptionHelper.CreateException<T>(message, args);
+            }
+        }
+
+    }
+
+
+}
