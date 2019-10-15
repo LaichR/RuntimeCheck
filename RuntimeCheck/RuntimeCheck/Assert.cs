e517c6e54eeae75dc624babd7c67237bfaf39dd9
LaichR <46196385+LaichR@users.noreply.github.com>
2019-10-15 11:51:27 +0200

    Some more comments
    - Description for class Assert
    - retrict usage of class assert to generate ViolatedAssertion exception

--- RuntimeCheck/RuntimeCheck/Assert.cs
+++ RuntimeCheck/RuntimeCheck/Assert.cs
@@ -3,21 +3,22 @@
 
 namespace RuntimeCheck
 {
+    /// <summary>
+    /// Assertions are used to specify conditions that must be met at a location in the program. This is a powerful tool to catch errors early.
+    /// Furthermore they serve as documentation for the code.
+    /// Traditionally assertions are removed when the development has completed in order to imporve performance. 
+    /// Even though in a final product assertions should not occure anymore i believe that many applications 
+    /// can afford keeping the the assertions alive. 
+    /// In contrast to the method Assert of the class System.Debug the class Assert only generates a Exception in case of a violated assertion. 
+    /// This is basically the same as with NUnit. But i do not want ot include NUnit in all my project and i 
+    /// </summary>
     public static class Assert
     {
         public static void True(bool condition, string message, params object[] args)
         {
             if (!condition)
             {
-                throw ExceptionHelper.CreateException<ViolatedAssertionException>(message, args);
-            }
-        }
-
-        public static void True<T>(bool condition, string message, params object[] args) where T:Exception
-        {
-            if(!condition)
-            {
-                throw ExceptionHelper.CreateException<T>(message, args);
+                throw new ViolatedAssertionException(string.Format( message, args));
             }
         }
 
