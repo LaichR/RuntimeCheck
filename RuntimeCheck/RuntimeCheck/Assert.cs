4b9b3e2887520651a50c450ae7e5a56700de3f9a
LaichR <46196385+LaichR@users.noreply.github.com>
2019-10-20 08:48:34 +0200

    skip the call frame of the function assert

--- RuntimeCheck/RuntimeCheck/Assert.cs
+++ RuntimeCheck/RuntimeCheck/Assert.cs
@@ -9,7 +9,7 @@ namespace RuntimeCheck
     /// Traditionally assertions are removed when the development has completed in order to imporve performance. 
     /// Even though in a final product assertions should not occure anymore i believe that many applications 
     /// can afford keeping the the assertions alive. 
-    /// In contrast to the method Assert of the class System.Debug the class Assert only generates a Exception in case of a violated assertion. 
+    /// In contrast to the method Assert of the class System.Debug the class Assert only generates an exception in case of a violated assertion. 
     /// This is basically the same as with NUnit. But i do not want ot include NUnit in all my project and i 
     /// </summary>
     public static class Assert
--- RuntimeCheck/RuntimeCheck/ViolatedAssertionException.cs
+++ RuntimeCheck/RuntimeCheck/ViolatedAssertionException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.Serialization;
 
 
@@ -8,15 +9,24 @@ namespace RuntimeCheck
     [Serializable]
     public class ViolatedAssertionException : SystemException
     {
-        public ViolatedAssertionException(string message)
-            : base(message) { }
+        const string StackTraceSerializationName = "AssertionStackTrace";
+        string _stackTrace;
+        internal ViolatedAssertionException(string message)
+            : base(message) {
+            var stack = new StackTrace(2); // i do not want to see the call frame of assert and this constructor!
+            _stackTrace = stack.ToString();
+        }
         protected ViolatedAssertionException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
+            : base(info, context) { _stackTrace = info.GetString(StackTraceSerializationName); }
 
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
+            info.AddValue(StackTraceSerializationName, _stackTrace);
         }
+
+        public override string StackTrace => _stackTrace;
+        
     }
 
 }
